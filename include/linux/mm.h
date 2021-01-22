2bfa9871ad59340708ccd56efda52fd70e85fb6d
Paul E. McKenney <paulmck@kernel.org>
2021-01-22 15:26:44 -0800

    Merge branches 'doc.2021.01.06a', 'fixes.2021.01.04b', 'kfree_rcu.2021.01.04a', 'mmdumpobj.2021.01.22a', 'nocb.2021.01.06a', 'rt.2021.01.04a', 'stall.2021.01.06a', 'torture.2021.01.12a' and 'tortureall.2021.01.06a' into HEAD
    doc.2021.01.06a: Documentation updates.
    fixes.2021.01.04b: Miscellaneous fixes.
    kfree_rcu.2021.01.04a: kfree_rcu() updates.
    mmdumpobj.2021.01.22a: Dump allocation point for memory blocks.
    nocb.2021.01.06a: RCU callback offload updates and cblist segment lengths.
    rt.2021.01.04a: Real-time updates.
    stall.2021.01.06a: RCU CPU stall warning updates.
    torture.2021.01.12a: Torture-test updates and polling SRCU grace-period API.
    tortureall.2021.01.06a: Torture-test script updates.

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -3167,5 +3167,7 @@ static inline int seal_check_future_write(int seals, struct vm_area_struct *vma)
 	return 0;
 }
 
+void mem_dump_obj(void *object);
+
 #endif /* __KERNEL__ */
 #endif /* _LINUX_MM_H */
